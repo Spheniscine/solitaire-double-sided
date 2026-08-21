d79909a0122b4544c4cf8ee92873fd35140caa46
Spheniscine <spheniscine@gmail.com>
2026-08-21 21:44:12 +0800

    CSS skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
+
+// const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+// altered version of KaTeX_Main to include filled "red" suits
+const KATEX_SUITS: Asset = asset!("/assets/KaTeX_Suits.woff2");
+
+// from https://www.confettijs.org/
+const CONFETTI_JS: Asset = asset!("/assets/confetti.min.js");
+
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 fn main() {
     dioxus::launch(App);
@@ -11,8 +19,37 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+        }
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: "anonymous",
+        }
+        document::Link {
+            href: "https://fonts.googleapis.com/css2?family=Noto+Emoji:wght@300..700&family=Noto+Sans+Symbols+2&family=Noto+Sans+Symbols:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap",
+            rel: "stylesheet",
+        }
         document::Link { rel: "icon", href: FAVICON }
+
+        document::Style {
+            // visibility hidden to prevent FOUC, is set back to visible in MAIN_CSS
+            r#"
+            html {{
+                visibility: hidden;
+            }}
+            @font-face {{
+                font-family: KaTeX_Suits;
+                font-style: normal;
+                font-weight: 700;
+                src: url({KATEX_SUITS}) format("woff2");
+            }} 
+            "#,
+        }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Script { src: CONFETTI_JS }
         Hero {}
 
     }
@@ -23,15 +60,15 @@ pub fn Hero() -> Element {
     rsx! {
         div {
             id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+            // img { src: HEADER_SVG, id: "header" }
+            // div { id: "links",
+            //     a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+            //     a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+            //     a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+            //     a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+            //     a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+            //     a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            // }
         }
     }
 }
