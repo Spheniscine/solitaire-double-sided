4bbf55d7f56cbb249d14aff08c586eadccb347b6
Spheniscine <spheniscine@gmail.com>
2026-08-30 14:34:19 +0800

    change to new anti-fouc

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,7 @@ const KATEX_SUITS: Asset = asset!("/assets/KaTeX_Suits.woff2");
 // from https://www.confettijs.org/
 const CONFETTI_JS: Asset = asset!("/assets/confetti.min.js");
 
-const STATIC_CSS: bool = !cfg!(debug_assertions);
-
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const MAIN_CSS_STR: &str = const_css_minify::minify!("../assets/main.css");
-const _: &str = include_str!("../assets/main.css"); // ensures recompilation if CSS changed
 
 fn main() {
     dioxus::launch(App);
@@ -52,17 +48,13 @@ fn App() -> Element {
             }} 
         "#,}
         
-        if STATIC_CSS {
-            document::Style { {MAIN_CSS_STR} }
-        } else {
-            // visibility hidden to prevent FOUC, is set back to visible in MAIN_CSS
-            document::Style {r#"
-                html {{
-                    visibility: hidden;
-                }}
-            "#,}
-            document::Link { href: MAIN_CSS, rel: "stylesheet" }
-        }
+        // visibility hidden to prevent FOUC, is set back to visible in MAIN_CSS
+        document::Style {r#"
+            #hero {{
+                visibility: hidden;
+            }}
+        "#,}
+        document::Link { href: MAIN_CSS, rel: "stylesheet" }
 
         document::Script { src: CONFETTI_JS }
         Hero {}
